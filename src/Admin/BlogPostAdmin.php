bf2f49708024a7429406924c7e2497f81a81b11b
lecocq <sgfdr>
2018-04-10 14:25:53 +0200

    3.2. Configuration du mappeur de formulaire

--- src/Admin/BlogPostAdmin.php
+++ src/Admin/BlogPostAdmin.php
@@ -6,12 +6,18 @@ namespace App\Admin;
 use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class BlogPostAdmin extends AbstractAdmin
 {
     protected function configureFormFields(FormMapper $formMapper)
     {
-        // ... configure $formMapper
+//        3.2. Configuration du mappeur de formulaire
+        $formMapper
+            ->add('title', TextType::class)
+            ->add('body', TextareaType::class)
+        ;
     }
 
     protected function configureListFields(ListMapper $listMapper)
